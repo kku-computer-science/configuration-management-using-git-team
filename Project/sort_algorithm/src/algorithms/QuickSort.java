431c9d685986f1759df39de2b9f7313ab64565d7
Phatcharida21 <Phatcharida.f@kkumail.com>
2025-12-10 16:28:43 +0700

    Create QuickSort.java

---
+++ Project/sort_algorithm/src/algorithms/QuickSort.java
@@ -0,0 +1,38 @@
+package algorithms;
+
+public class QuickSort implements ISortStrategy {
+
+    @Override
+    public int[] sort(int[] data) {
+        quickSort(data, 0, data.length - 1);
+        return data;
+    }
+
+    private void quickSort(int[] arr, int low, int high) {
+        if (low < high) {
+            int pi = partition(arr, low, high);
+            quickSort(arr, low, pi - 1);
+            quickSort(arr, pi + 1, high);
+        }
+    }
+
+    private int partition(int[] arr, int low, int high) {
+        int pivot = arr[high];
+        int i = low - 1;
+
+        for (int j = low; j < high; j++) {
+            if (arr[j] <= pivot) {
+                i++;
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+
+        int temp = arr[i + 1];
+        arr[i + 1] = arr[high];
+        arr[high] = temp;
+
+        return i + 1;
+    }
+}

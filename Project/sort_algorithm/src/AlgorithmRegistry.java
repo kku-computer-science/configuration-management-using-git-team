55808196c9947a3fc1c2a0ffd46da30546a8c62b
Ekkawich Pilawan <ekkawich.p@kkumail.com>
2025-12-10 17:24:36 +0700

    Register Quick Sort and Bubble Sort to AlgorithmRegistry.java

--- Project/sort_algorithm/src/AlgorithmRegistry.java
+++ Project/sort_algorithm/src/AlgorithmRegistry.java
@@ -1,12 +1,16 @@
 import java.util.HashMap;
 import java.util.Map;
 
+import algorithms.BubbleSort;
 import algorithms.ISortStrategy;
+import algorithms.QuickSort;
 
 public class AlgorithmRegistry {
     private static final Map<String, ISortStrategy> algorithms = new HashMap<>();
 
     static {
+        algorithms.put("Quick", new QuickSort());
+        algorithms.put("Bubble", new BubbleSort());
     }
 
     public static ISortStrategy get(String name) {
